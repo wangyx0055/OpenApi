ba5f46bf8285a9d2be0ebde19a0a430ac0239315
zanyun <office@zanyun.com>
2014-10-08 22:43:00 +0800

    格式

--- ZanYun.OpenApi/Tencent/ExMail/Entity.cs
+++ ZanYun.OpenApi/Tencent/ExMail/Entity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ZanYun.OpenApi.Tencent.ExMail
 {
-    class Entity
+    public class Entity
     {
     }
 }
--- ZanYun.OpenApi/Tencent/ExMail/Utility.cs
+++ ZanYun.OpenApi/Tencent/ExMail/Utility.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ZanYun.OpenApi.Tencent.ExMail
 {
-    class Utility
+    public class Utility
     {
     }
 }
