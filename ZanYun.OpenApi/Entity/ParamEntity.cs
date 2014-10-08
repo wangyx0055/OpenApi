962982d12b1d26ab37861c309282e994d2e383a8
zanyun <office@zanyun.com>
2014-10-08 22:33:07 +0800

    增加模拟Post公共方法

---
+++ ZanYun.OpenApi/Entity/ParamEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZanYun.OpenApi.Entity
+{
+    public class ParamEntity
+    {
+        /// <summary>
+        /// 提交URL
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 参数集
+        /// </summary>
+        public NameValueCollection NVCollection { get; set; }
+
+        /// <summary>
+        /// 头信息
+        /// </summary>
+        public string Headers { get; set; }
+    }
+}
--- ZanYun.OpenApi/Utility/WebHelper.cs
+++ ZanYun.OpenApi/Utility/WebHelper.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +11,54 @@ namespace ZanYun.OpenApi.Utility
 {
     public class WebHelper
     {
+        /// <summary>
+        /// 模拟POST提交
+        /// </summary>
+        /// <typeparam name="T">实体</typeparam>
+        /// <param name="postParam">参数</param>
+        /// <returns></returns>
+        public static T Post<T>(Entity.ParamEntity postParam)
+        {
+            try
+            {
+                string stringInfo = Post(postParam);
+                if (!string.IsNullOrEmpty(stringInfo))
+                {
+                    return JsonConvert.DeserializeObject<T>(stringInfo);
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
 
-
+        /// <summary>
+        /// 模拟POST提交
+        /// </summary>
+        /// <param name="postParam">参数</param>
+        /// <returns></returns>
+        public static string Post(Entity.ParamEntity postParam)
+        {
+            WebClient webClient = new WebClient();
+            try
+            {
+                if (!string.IsNullOrEmpty(postParam.Headers))
+                {
+                    webClient.Headers.Add(postParam.Headers);
+                }
+                byte[] byteInfo = webClient.UploadValues(postParam.Url, "POST", postParam.NVCollection);
+                string stringInfo = System.Text.Encoding.UTF8.GetString(byteInfo);
+                return stringInfo;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
